29a8c95b752bb5604d1f73b352479ef514df8b8e
hoang <pham.ngoc.hoang@linecorp.com>
2025-03-14 12:39:45 +0700

    Add new APIs for integrating with URL
    We have added several APIs to support users integrating with Promgen's URL:
    - List and filter URLs.
    - List and filter URL Probes.
    - Delete an existing URL.

--- promgen/filters.py
+++ promgen/filters.py
@@ -205,3 +205,33 @@ class ExporterFilter(django_filters.rest_framework.FilterSet):
         field_name="enabled",
         help_text="Filter by enabled status (true or false). Example: enabled=true",
     )
+
+
+class ProbeChoices:
+    def __iter__(self):
+        return iter(models.Probe.objects.values_list("module", "description"))
+
+    def __len__(self):
+        return models.Probe.objects.values("module").count()
+
+
+class URLFilter(django_filters.rest_framework.FilterSet):
+    project = django_filters.CharFilter(
+        field_name="project__name",
+        lookup_expr="contains",
+        help_text="Filter by project name containing a specific substring. "
+        "Example: project=Example Project",
+    )
+    probe = django_filters.ChoiceFilter(
+        field_name="probe__module",
+        choices=ProbeChoices(),
+        help_text="Filter by exact probe scheme. Example: probe=http_2xx",
+    )
+
+
+class ProbeFilter(django_filters.rest_framework.FilterSet):
+    module = django_filters.CharFilter(
+        field_name="module",
+        lookup_expr="contains",
+        help_text="Filter by probe module containing a specific substring. Example: probe=http_2xx",
+    )
